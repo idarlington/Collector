003b18883a4f5f3e68e158e069f9ea4161d83883
Ikenna Darlington Ogbajie <iogbajie@gmail.com>
2018-01-30 13:41:43 +0100

    Convert String to Type

--- src/main/scala/com/lunatech/collector/StreamActor.scala
+++ src/main/scala/com/lunatech/collector/StreamActor.scala
@@ -1,17 +1,17 @@
 package com.lunatech.collector
 
-import akka.actor.{ Actor, ActorLogging }
+import akka.actor.{Actor, ActorLogging}
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model._
-import akka.stream.{ ActorMaterializer, ActorMaterializerSettings }
-import akka.util.ByteString
+import akka.http.scaladsl.unmarshalling.{Unmarshal}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import akka.pattern.pipe
 
-final case class Vehicle(id: String)
 
-class StreamActor extends Actor
-  with ActorLogging {
 
-  import akka.pattern.pipe
+class StreamActor extends Actor with ActorLogging with JsonFormatSupport {
+
   import context.dispatcher
 
   final implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(context.system))
@@ -19,13 +19,12 @@ class StreamActor extends Actor
   val http = Http(context.system)
   val URL = "http://api.metro.net/agencies/lametro/vehicles/"
 
-  override def preStart() = {}
-
   def receive = {
-    case HttpResponse(StatusCodes.OK, headers, entity, _) =>
-      entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach { body =>
-        log.info("Got response, body: " + body.utf8String)
+    case HttpResponse(StatusCodes.OK, headers, entity, _) => {
+      Unmarshal(entity).to[VehicleList].map { jsonString =>
+        println(jsonString)
       }
+    }
     case resp @ HttpResponse(code, _, _, _) =>
       log.info("Request failed, response code: " + code)
       resp.discardEntityBytes()
