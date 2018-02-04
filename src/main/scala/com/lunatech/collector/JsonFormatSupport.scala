83d307993c9e0b74c857d3d14c5b44836d1b327d
Ikenna Darlington Ogbajie <iogbajie@gmail.com>
2018-02-04 05:04:21 +0100

    Add Database Connection

--- src/main/scala/com/lunatech/collector/JsonFormatSupport.scala
+++ src/main/scala/com/lunatech/collector/JsonFormatSupport.scala
@@ -2,6 +2,8 @@ package com.lunatech.collector
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 
+// TODO change id to long
+
 abstract class VehicleBuilder(
 	id : String,
 	heading : Double,
@@ -13,30 +15,30 @@ abstract class VehicleBuilder(
 	predictable : Boolean
 )
 
+final case class Vehicle(
+	id : String,
+	heading : Double,
+	latitude : Double,
+	longitude : Double,
+	run_id : String,
+	route_id : String,
+	seconds_since_report : Int,
+	predictable : Boolean
+)
+		extends VehicleBuilder(
+			id : String,
+			heading,
+			latitude,
+			longitude,
+			run_id,
+			route_id,
+			seconds_since_report,
+			predictable
+		)
+
+final case class VehicleList( items : List[ Vehicle ] )
+
 trait JsonFormatSupport extends SprayJsonSupport {
-
-	final case class Vehicle(
-		id : String,
-		heading : Double,
-		latitude : Double,
-		longitude : Double,
-		run_id : String,
-		route_id : String,
-		seconds_since_report : Int,
-		predictable : Boolean
-	)
-			extends VehicleBuilder(
-				id : String,
-				heading,
-				latitude,
-				longitude,
-				run_id,
-				route_id,
-				seconds_since_report,
-				predictable
-			)
-
-	final case class VehicleList( items : List[ Vehicle ] )
 
 	import spray.json.DefaultJsonProtocol._
 
